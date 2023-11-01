4740464fc81c7aaa4373cf9d2d3f3701faf45b6e
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-10-31 23:06:40 -0300

    aula 02.08 - completando...

--- fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -8,6 +8,25 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+$looping = 1;
+$while = [];
+
+while($looping <= 5){
+    $while[] = $looping;
+    $looping++;
+}
+var_dump($while);
+
+$looping = 5;
+$while = [];
+
+do {
+    $while[] = $looping;
+    $looping--;
+} while ($looping >=1);
+var_dump($while);
+
+//3 min e 27
 
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php

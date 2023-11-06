2762e3a2778e58c46b44edab7510d7d2eaa82ed8
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-05 23:10:15 -0300

    Aula - 02.10 - Requisição de arquivos

--- fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -8,6 +8,26 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
 
+//include "File.php";
+//echo "<p> Continue</p>";
+
+//include "header.php";
+include __DIR__."/header.php";
+
+$profile = new StdClass();
+$profile->name = "Guilherme";
+$profile->age = 18;
+$profile->email = "Guilherme@gmail.com";
+//echo "<pre>";
+//var_dump($profile);
+////echo "<pre>";
+include __DIR__."/profile.php";
+
+$profile = new StdClass();
+$profile->name = "Shake";
+$profile->age = 7;
+$profile->email = "shake@gmail.com";
+include_once __DIR__."/profile.php";
 
 /*
  * [ require ] https://php.net/manual/pt_BR/function.require.php
@@ -15,3 +35,12 @@ fullStackPHPClassSession("include, include_once", __LINE__);
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
 
+//require "File.php";
+//echo "<p> Continue</p>";
+
+require __DIR__."/config.php";
+
+
+echo "<h1>" . COURSE . "</h1>";
+
+var_dump(require_once __DIR__."/config.php");

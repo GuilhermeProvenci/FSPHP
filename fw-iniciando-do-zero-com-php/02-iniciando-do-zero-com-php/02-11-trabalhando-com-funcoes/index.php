397a88a49b57f911fd4b3c8ccf556f0eae0a4a3a
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-07 21:28:48 -0300

    Aula - 02.11 - Trabalhando com funções

--- fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ fw-iniciando-do-zero-com-php/02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -7,11 +7,34 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  */
 fullStackPHPClassSession("functions", __LINE__);
 
+require __DIR__ ."/functions.php";
+echo '<pre>';
+var_dump(functionName("argumento 1", "argumento 2","argumento3"));
+echo '</pre>';
+echo '<pre>';
+var_dump(functionName("argumento 4", "argumento 5","argumento6"));
+echo '</pre>';
 
+
+echo '<pre>';
+var_dump(optionArgus("argumento 4"));
+echo '</pre>';
+
+echo '<pre>';
+var_dump(optionArgus("argumento 4", "argumento 5"));
+echo '</pre>';
+
+echo '<pre>';
+var_dump(optionArgus("argumento 4", "argumento 5","argumento6"));
+echo '</pre>';
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
+
+$weight = 75;
+$height = 1.70;
+echo calcImc();
 
 
 /*
@@ -19,8 +42,14 @@ fullStackPHPClassSession("global access", __LINE__);
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(200);
+$pay = payTotal(300);
+echo $pay;
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+echo '<pre>';
+var_dump(myTeam("Guilherme", "Lanna"));
+echo '</pre>';

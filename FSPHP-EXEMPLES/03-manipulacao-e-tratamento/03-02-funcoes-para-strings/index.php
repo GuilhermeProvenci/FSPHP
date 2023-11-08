deb87b4ef90220695c3a0348ef04d8a8e0de1a87
Guilherme Provenci de Lima <guilhermelimamp2020@gmail.com>
2023-11-08 17:54:16 -0300

    Em andamento...

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
@@ -7,11 +7,35 @@ fullStackPHPClassName("03.02 - Funções para strings");
  */
 fullStackPHPClassSession("strings e multibyte", __LINE__);
 
+$string = "O último show do AC/DC foi Incrível!";
+
+echo "<pre>";
+var_dump([
+    "string" => $string,
+    "strlen" => strlen($string),
+    "mb_strlen" => mb_strlen($string),
+    "substr" => substr($string, "9"),
+    "mb_substr" => mb_substr($string, "9"),
+    "strtoupper" => strtoupper($string),
+    "mb_strtoupper" => mb_strtoupper($string),
+]);
+
+
 
 /**
  * [ conversão de caixa ] https://php.net/manual/en/function.mb-convert-case.php
  */
 fullStackPHPClassSession("conversão de caixa", __LINE__);
+
+$mbString = $string;
+
+var_dump([
+    "mb_strtoupper" => mb_strtoupper($string),
+    "mb_strtolower" => mb_strtolower($string),
+    "mb_convert_case UPPER" => mb_convert_case($mbString, MB_CASE_UPPER),
+    "mb_convert_case LOWER" => mb_convert_case($mbString, MB_CASE_LOWER),
+    "mb_convert_case TITLE" => mb_convert_case($mbString, MB_CASE_TITLE),
+]);
 
 
 /**
@@ -19,8 +43,23 @@ fullStackPHPClassSession("conversão de caixa", __LINE__);
  */
 fullStackPHPClassSession("substituição", __LINE__);
 
+$mbReplace = $mbString . " Fui, iria novamente, e foi épico!";
+
+var_dump([
+    "mb_strlen" => mb_strlen($mbReplace),
+    "mb_strpos" => mb_strpos($mbReplace, ", "),
+    "mb_strrpos" => mb_strrpos($mbReplace, ", "),
+    "mb_substr" => mb_substr($mbReplace, 40 + 2, 14),
+    "mb_strstr" => mb_strstr($mbReplace, ", ", true),
+    "mb_strrchr" => mb_strrchr($mbReplace, ", ", true),
+
+]);
+
 
 /**
  * [ parse string ] parse_str | mb_parse_str
  */
 fullStackPHPClassSession("parse string", __LINE__);
+
+
+echo "</pre>";

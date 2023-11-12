d1d150c33209a535765bb7ee065c98d5592503fd
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-11 23:08:53 -0300

    Aula - 03.04 - Manipulação de objetos

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -1,14 +1,63 @@
 <?php
-require __DIR__ . '/../../fullstackphp/fsphp.php';
+require __DIR__ . '/../../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.04 - Manipulação de objetos");
-
+echo "<pre>";
 /*
  * [ manipulação ] http://php.net/manual/pt_BR/language.types.object.php
  */
 fullStackPHPClassSession("manipulação", __LINE__);
+
+$arrProfile = [
+  "name" => "Guilherme",
+  "company" => "GPL",
+  "mail" => "gui@gmail.com"
+];
+$objProfile = (object)$arrProfile;
+//var_dump($arrProfile, $objProfile);
+
+echo "<p> {$arrProfile['name']} trabalha na {$arrProfile['company']} </p>";
+echo "<p> {$objProfile->name} trabalha na {$objProfile->company} </p>";
+
+$ceo = $objProfile;
+unset($ceo->company);
+var_dump($ceo);
+
+$company = new StdClass();
+$company->company = "GPL";
+$company->ceo = $ceo;
+$company->manager = new StdClass();
+$company->manager->name = "Shake";
+$company->manager->mail = "Shake@gmail.com";
+
+var_dump($company);
 
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+$date = new DateTime();
+
+var_dump([
+    "class" => get_class($company),
+    "methods" => get_class_methods($date),
+    "vars" => get_object_vars($date),
+    "parent" => get_parent_class($date),
+    "subclass" => is_subclass_of($date, "DateTime")
+]);
+
+$exception = new PDOException();
+
+
+echo " <h2>----------------EXCEPTION----------------</h2>";
+var_dump([
+    "class" => get_class($exception),
+    "methods" => get_class_methods($exception),
+    "vars" => get_object_vars($exception),
+    "parent" => get_parent_class($exception),
+    "subclass" => is_subclass_of($exception, "Exception")
+]);
+
+
+echo "</pre>";

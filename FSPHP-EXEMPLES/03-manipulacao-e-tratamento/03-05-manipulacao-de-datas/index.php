485350a5b8e4717710a275894354c538e2e780c4
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-12 23:12:23 -0300

    em andamento...

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-05-manipulacao-de-datas/index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/../../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.05 - Manipulação de datas");
-
+echo "<pre>";
 /*
  * [ a função date ] setup | output
  * [ date ] https://php.net/manual/pt_BR/function.date.php
@@ -9,8 +9,33 @@ fullStackPHPClassName("03.05 - Manipulação de datas");
  */
 fullStackPHPClassSession("a função date", __LINE__);
 
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_W3C),
+    date("d/m/y H:i:s")
+]);
+
+define("DATE_BR", "d/m/y H:i:s");
+define("DATE_TIMEZONE", "Pacific/Apia");
+
+date_default_timezone_set(DATE_TIMEZONE);
+
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_BR)
+]);
+
+var_dump(getdate());
+
+echo "<p>Hoje é dia ", getdate()["mday"], "!</p>";
 
 /**
  * [ string to date ] strtotime | strftime
  */
 fullStackPHPClassSession("string to date", __LINE__);
+
+var_dump([
+    "strtotime NOW" => strtotime("now")
+]);
+
+echo "</pre>";

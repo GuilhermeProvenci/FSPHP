a31e0b96e32cdb7868c68203d20b59ae26d29740
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-13 23:19:30 -0300

    Aula - 03.06 - Uma classe DateTime

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-06-uma-classe-datetime/index.php
@@ -1,12 +1,39 @@
 <?php
 require __DIR__ . '/../../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.06 - Uma classe DateTime");
-
+echo "<pre>";
 /*
  * [ DateTime ] http://php.net/manual/en/class.datetime.php
  */
 fullStackPHPClassSession("A classe DateTime", __LINE__);
 
+define("DATE_BR", "d/m/y H:i:s");
+
+$dateNow = new DateTime();
+$dateBirth = new DateTime("2005-08-22");
+$dateStatic = DateTime::createFromFormat("d/m/Y", "10/03/2010");
+
+var_dump(
+    $dateNow,
+    $dateBirth,
+    $dateStatic
+);
+
+var_dump([
+    $dateNow->format(DATE_BR),
+    $dateNow->format("d")
+]);
+
+echo "<p>Hoje é dia {$dateNow->format("d")} do {$dateNow->format("m")} de {$dateNow->format("Y")}</p>";
+
+$newTimeZone = new DateTimeZone("Pacific/Apia");
+$newDateTime = new DateTime("now", $newTimeZone);
+
+var_dump([
+    $newTimeZone,
+    $newDateTime,
+    $dateNow
+]);
 
 /*
  * [ DateInterval ] http://php.net/manual/en/class.dateinterval.php
@@ -14,8 +41,59 @@ fullStackPHPClassSession("A classe DateTime", __LINE__);
  */
 fullStackPHPClassSession("A classe DateInterval", __LINE__);
 
+$dateInterval = new DateInterval("P10Y2MT10M");
+var_dump($dateInterval);
+
+$dateTime = new DateTime("now");
+//$dateTime->add($dateInterval);
+$dateTime->sub($dateInterval);
+
+var_dump($dateTime);
+
+
+$birth = new DateTime(date("Y") . "-08-22");
+$dateNow = new DateTime("now");
+
+$diff = $dateNow->diff($birth);
+
+var_dump($birth, $diff);
+
+if ($diff->invert) {
+    echo "<p> seu aniversário foi a {$diff->days} dias.</p>";
+} else {
+    echo "<p> faltam {$diff->days} dias para o seu aniversário.</p>";
+}
+
+$dateResources = new DateTime ("now");
+var_dump([
+    $dateResources->format(DATE_BR),
+    $dateResources->sub(DateInterval::createFromDateString("10days"))->format(DATE_BR),
+    $dateResources->add(DateInterval::createFromDateString("20days"))->format(DATE_BR),
+]);
+
 
 /**
  * [ DatePeriod ] http://php.net/manual/pt_BR/class.dateperiod.php
  */
 fullStackPHPClassSession("A classe DatePeriod", __LINE__);
+
+$start = new DateTime("now");
+$interval = new DateInterval("P1M");
+$end = new DateTime("2025-01-01");
+
+$period = new DatePeriod($start, $interval, $end);
+
+var_dump([
+    $start->format(DATE_BR),
+    $interval,
+    $end->format(DATE_BR)
+], $period, get_class_methods($period));
+
+
+echo "<h1>Sua assinatura </h1>";
+foreach ($period as $recurrences) {
+    echo "<p>Próximo vencimento {$recurrences->format(DATE_BR)}</p>";
+}
+
+
+echo "</pre>";

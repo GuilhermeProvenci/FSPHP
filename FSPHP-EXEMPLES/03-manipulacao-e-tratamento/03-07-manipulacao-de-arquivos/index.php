264eaf514d16f51152db916838360928e6e01310
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-21 23:51:01 -0300

    Aula - 03.07 - Manipulação de arquivos

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-07-manipulacao-de-arquivos/index.php
@@ -21,8 +21,51 @@ if(file_exists($file) && is_file($file)){
  */
 fullStackPHPClassSession("leitura e gravação", __LINE__);
 
+if (!file_exists($file) || !is_file($file)) {
+    $fileOpen = fopen($file, "w");
+    fwrite($fileOpen, "Linha 01" . PHP_EOL);
+    fwrite($fileOpen, "Linha 02" . PHP_EOL);
+    fwrite($fileOpen, "Linha 03" . PHP_EOL);
+    fwrite($fileOpen, "Lorem Ipsum is simply dummy text of the printing and typesetting industry." . PHP_EOL);
+    fclose($fileOpen);
+}else {
+    var_dump(
+        file($file),
+        pathinfo($file)
+    );
+}
+$fileContent = file($file);
+echo "<p>", $fileContent[2], "</p>";
+
+$open = fopen($file, "r");
+while (!feof($open)) {
+    echo "<p>" . fgets($open)."</p>";
+}
+
+fclose($open);
+
 
 /*
  * [ get, put content ] file_get_contents | file_put_contents
  */
 fullStackPHPClassSession("get, put content", __LINE__);
+
+
+$getContents = __DIR__ . "/teste2.txt";
+
+if (file_exists($getContents) && is_file($getContents)) {
+    echo file_get_contents($getContents);
+}else{
+    $data = "<article><h1><p>Guilherme Lima<br>Developer</p></h1></article";
+    file_put_contents($getContents, $data);
+    echo file_get_contents($getContents);
+}
+
+//unlink($getContents);
+//unlink($file);
+
+if(file_exists(__DIR__."/teste3.txt") && is_file(__DIR__ . "/teste3.txt")){
+    unlink(__DIR__ . "/teste3.txt");
+}
+
+echo "</pre>";

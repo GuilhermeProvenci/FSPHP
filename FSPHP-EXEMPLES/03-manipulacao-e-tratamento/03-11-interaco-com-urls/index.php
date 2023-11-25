4c6c503b030f3fc67e6cc6b9e83456c2c2211a94
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-11-25 15:05:37 -0300

    Aula - 03.11 - Interação com URLs

--- FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
+++ FSPHP-EXEMPLES/03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
@@ -1,11 +1,33 @@
 <?php
 require __DIR__ . '/../../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.11 - Interação com URLs");
-
+echo "<pre>";
 /*
  * [ argumentos ] ?[&[&][&]]
  */
 fullStackPHPClassSession("argumentos", __LINE__);
+
+
+echo "<h1><a href='index.php'> Clear </a> </h1>";
+echo "<p><a href='index.php?arg1=true&arg2=true'>Argumentos</a></p>";
+
+
+$data = [
+  "name" => "Guilherme",
+  "company" => "Gpl",
+  "email" => "guilherme@gmail.com"
+];
+
+$arguments = http_build_query($data);
+echo "<p><a href='index.php?{$arguments}'> Args By Array</a></p>";
+
+var_dump($_GET);
+
+$object = (object) $data;
+var_dump(
+  $object,
+  http_build_query($object)
+);
 
 
 /*
@@ -13,3 +35,58 @@ fullStackPHPClassSession("argumentos", __LINE__);
  * [ filter list ] https://php.net/manual/en/filter.filters.php
  */
 fullStackPHPClassSession("segurança", __LINE__);
+
+
+$dataFilter = http_build_query([
+  "name" => "Guilherme",
+  "company" => "Gpl",
+  "mail" => "guilherme@gmail.com",
+  "site" => "GPL.com.br",
+  "script" => "<script>alert('Esse é um JavaScritp')</script>"
+]);
+
+echo "<p><a href='index.php?{$dataFilter}'>DataFilter</a></p>";
+
+$dataUrl = filter_input_array(INPUT_GET, FILTER_DEFAULT);
+
+if ($dataUrl) {
+  if (in_array("", $dataUrl)) {
+    echo "<p class='trigger warning'>Faltam Dados</p>";
+  } elseif (empty($dataUrl["mail"])) {
+    echo "<p class='trigger warning'>Falta o E-mail</p>";
+  } elseif (!filter_var($dataUrl["mail"], FILTER_VALIDATE_EMAIL)) {
+    echo "<p class='trigger warning'>E-mail inválido</p>";
+  } else {
+    echo "<p class='trigger accept'>Tudo Certo</p>";
+  }
+} else {
+  var_dump(false);
+}
+
+var_dump($dataUrl);
+
+
+$dataFilter = http_build_query([
+  "name" => "Guilherme",
+  "company" => "Gpl",
+  "mail" => "guilherme@gmail.com",
+  "site" => "http://GPL.com.br",
+  "script" => "<script>alert('Esse é um JavaScritp')</script>"
+]);
+
+parse_str($dataFilter, $arrDataFilter);
+var_dump($arrDataFilter);
+
+
+$dataPreFilter = [
+  "name" => FILTER_SANITIZE_STRING,
+  "company" => FILTER_SANITIZE_STRING,
+  "mail" => FILTER_VALIDATE_EMAIL,
+  "site" => FILTER_VALIDATE_DOMAIN,
+  "script" => FILTER_SANITIZE_STRING
+];
+
+
+var_dump(filter_var_array($arrDataFilter, $dataPreFilter));
+
+echo "</pre>";
